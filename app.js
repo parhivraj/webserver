7b3a74e3128b771d339b4e029fbca7895b83e181
Your Name <you@example.com>
2022-07-10 15:43:23 +0000

    updated data

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ angular.module('sortApp', [])
   
   // create the list of sushi rolls 
   $scope.sushi = [
-    { name: 'Cali Roll', fish: 'Ashis', tastiness: 67 },
-    { name: 'Philly', fish: 'Dipu', tastiness: 78 },
+    { name: 'Cali Roll', fish: 'Rohit', tastiness: 67 },
+    { name: 'Philly', fish: 'Aron', tastiness: 78 },
     { name: 'Tiger', fish: 'Dinesh', tastiness: 80 },
     { name: 'Rainbow', fish: 'Karthik', tastiness: 83 },
     
