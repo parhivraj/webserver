2002021ca714fa93a2f3f0302c964b5cd2d9a34c
root <root@ip-172-31-86-99.ec2.internal>
2022-07-10 06:13:38 +0000

    my Application

---
+++ app.js
@@ -0,0 +1,24 @@
+// app.js
+angular.module('sortApp', [])
+
+.controller('mainController', function($scope) {
+  $scope.sortType     = 'name'; // set the default sort type
+  $scope.sortReverse  = false;  // set the default sort order
+  $scope.searchFish   = '';     // set the default search/filter term
+  
+  // create the list of sushi rolls 
+  $scope.sushi = [
+    { name: 'Cali Roll', fish: 'Ashis', tastiness: 2 },
+    { name: 'Philly', fish: 'Dipu', tastiness: 4 },
+    { name: 'Tiger', fish: 'Arun', tastiness: 7 },
+    { name: 'Rainbow', fish: 'Variety', tastiness: 6 },
+    
+    { name: 'Philly1', fish: 'Tuna1', tastiness: 41 },
+    { name: 'Tiger1', fish: 'Eel1', tastiness: 71 },
+    { name: 'Rainbow1', fish: 'Variety1', tastiness: 63 },
+    { name: 'Philly3', fish: 'Tuna1', tastiness: 43 },
+    { name: 'Tiger3', fish: 'Eel1', tastiness: 71 },
+    { name: 'Rainbow3', fish: 'Variety1', tastiness: 63 }
+  ];
+  
+});
